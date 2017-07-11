c75b2e1ceba21792835177f2856296e3c88aef62
Jens Rehsack <sno@netbsd.org>
2017-07-11 11:06:09 +0200

    inline/btree_{insert,remove}.pm: new tests
    Add some tests for insert and remove items into and from a sorted list
    searchable via binsearch.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

---
+++ inline/btree_remove.pm
@@ -0,0 +1,81 @@
+
+use Test::More;
+use Test::LMU;
+
+my @even = map { $_ * 2 } 1 .. 100;
+my @odd  = map { $_ * 2 - 1 } 1 .. 100;
+my @expected;
+
+@expected = @even;
+@in = mesh @odd, @even;
+foreach my $v (@odd) { is( $v, btree_remove { $_ <=> $v } @in) }
+is_deeply(\@in, \@expected, "btree_remove all odd elements succeeded");
+
+@in = mesh @odd, @even;
+foreach my $v (reverse @odd) { is( $v, btree_remove { $_ <=> $v } @in) }
+is_deeply(\@in, \@expected, "btree_remove all odd elements succeeded");
+
+@expected = @odd;
+@in = mesh @odd, @even;
+foreach my $v (@even) { is( $v, btree_remove { $_ <=> $v } @in); }
+is_deeply(\@in, \@expected, "btree_remove all even elements succeeded");
+
+@in = mesh @odd, @even;
+foreach my $v (reverse @even) { is( $v, btree_remove { $_ <=> $v } @in); }
+is_deeply(\@in, \@expected, "btree_remove all even elements succeeded");
+
+leak_free_ok(
+    'btree_remove first' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[0];
+        btree_remove { $_ <=> $v } @list
+    },
+    'btree_remove last' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[-1];
+        btree_remove { $_ <=> $v } @list
+    },
+    'btree_remove middle' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[int($#list/2)];
+        btree_remove { $_ <=> $v } @list
+    },
+);
+
+leak_free_ok(
+    'btree_remove first with stack-growing' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[0];
+        btree_remove { grow_stack(); $_ <=> $v } @list
+    },
+    'btree_remove last with stack-growing' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[-1];
+        btree_remove { grow_stack(); $_ <=> $v } @list
+    },
+    'btree_remove middle with stack-growing' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[int($#list/2)];
+        btree_remove { grow_stack(); $_ <=> $v } @list
+    },
+);
+
+leak_free_ok(
+    'btree_remove first with stack-growing and exception' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[0];
+        eval { btree_remove { grow_stack(); $_ <=> $v or die "Goal!"; $_ <=> $v } @list };
+    },
+    'btree_remove last with stack-growing and exception' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[-1];
+        eval { btree_remove { grow_stack(); $_ <=> $v or die "Goal!"; $_ <=> $v } @list };
+    },
+    'btree_remove middle with stack-growing and exception' => sub {
+        my @list = mesh @odd, @even;
+	my $v = $list[int($#list/2)];
+        eval { btree_remove { grow_stack(); $_ <=> $v or die "Goal!"; $_ <=> $v } @list };
+    },
+);
+
+done_testing;
