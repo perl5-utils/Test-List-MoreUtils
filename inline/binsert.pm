469b5680251f708cea65f5dd0fb1840b49191bd1
Jens Rehsack <sno@netbsd.org>
2017-07-25 15:43:45 +0200

    inline/binsert.pm: improve test coverage
    Add tests to prove stability of insert into sorted list using binary search
    algorithms.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- inline/binsert.pm
+++ inline/binsert.pm
@@ -11,24 +11,72 @@ my @expected;
 foreach my $v (@odd) { binsert { $_ <=> $v } $v, @in }
 is_deeply(\@in, \@expected, "binsert odd elements into even list succeeded");
 
+@in = @even;
+@expected = mesh @odd, @even;
+foreach my $v (reverse @odd) { binsert { $_ <=> $v } $v, @in }
+is_deeply(\@in, \@expected, "binsert odd elements reversely into even list succeeded");
+
 @in = @odd;
 foreach my $v (@even) { binsert { $_ <=> $v } $v, @in }
 is_deeply(\@in, \@expected, "binsert even elements into odd list succeeded");
 
+@in = @odd;
+foreach my $v (reverse @even) { binsert { $_ <=> $v } $v, @in }
+is_deeply(\@in, \@expected, "binsert even elements reversely into odd list succeeded");
+
+@in = @even;
+@expected = map { $_, $_ } @in;
+foreach my $v (@even) { binsert { $_ <=> $v } $v, @in }
+is_deeply(\@in, \@expected, "binsert existing even elements into even list succeeded");
+
+@in = @even;
+@expected = map { $_, $_ } @in;
+foreach my $v (reverse @even) { binsert { $_ <=> $v } $v, @in }
+is_deeply(\@in, \@expected, "binsert existing even elements reversely into even list succeeded");
+
 leak_free_ok(
-    binsert => sub {
+    'binsert random' => sub {
         my @list = map { $_ * 2 } 1 .. 100;
         my $elem = int( rand(100) ) + 1;
         binsert { $_ <=> $elem } $elem, @list;
-    }
+    },
+    'binsert existing random' => sub {
+        my @list = map { $_ * 2 } 1 .. 100;
+        my $elem = 2* ( int( rand(100) ) + 1 );
+        binsert { $_ <=> $elem } $elem, @list;
+    },
+    'binsert odd into even' => sub {
+        my @list = @even;
+        foreach my $elem (@odd) { binsert { $_ <=> $elem } $elem, @list; }
+    },
+    'binsert even into odd' => sub {
+        my @list = @odd;
+        foreach my $elem (@even) { binsert { $_ <=> $elem } $elem, @list; }
+    },
+    'binsert odd into odd' => sub {
+        my @list = @odd;
+        foreach my $elem (@odd) { binsert { $_ <=> $elem } $elem, @list; }
+    },
+    'binsert even into even' => sub {
+        my @list = @even;
+        foreach my $elem (@even) { binsert { $_ <=> $elem } $elem, @list; }
+    },
 );
 
 leak_free_ok(
-    'binsert with stack-growing' => sub {
+    'binsert random with stack-growing' => sub {
         my @list = map { $_ * 2 } 1 .. 100;
         my $elem = int( rand(100) ) + 1;
         binsert { grow_stack(); $_ <=> $elem } $elem, @list;
-    }
+    },
+    'binsert odd with stack-growing' => sub {
+        my @list = @even;
+        foreach my $elem (@odd) { binsert { grow_stack(); $_ <=> $elem } $elem, @list; }
+    },
+    'binsert even with stack-growing' => sub {
+        my @list = @odd;
+        foreach my $elem (@even) { binsert { grow_stack(); $_ <=> $elem } $elem, @list; }
+    },
 );
 
 leak_free_ok(
