90162a3074946dea2d5c5e33f76e8ab13fe22b8e
Jens Rehsack <sno@netbsd.org>
2020-10-02 08:18:22 +0200

    {inline,lib}/: apply Perl::Tidy rules
    Apply Perl::Tidy rules with most recent Perl::Tidy (v20201001).
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- inline/slideatatime.pm
+++ inline/slideatatime.pm
@@ -15,7 +15,7 @@ while (my @vals = $it->())
 is(is_deeply(\@r, ['a b c', 'd e f', 'g']), 1, "slideatatime as natatime with 3 elements");
 
 $it = slideatatime 2, 3, @x;
-@r = ();
+@r  = ();
 while (my @vals = $it->())
 {
     push @r, "@vals";
@@ -23,7 +23,7 @@ while (my @vals = $it->())
 is(is_deeply(\@r, ['a b c', 'c d e', 'e f g', 'g']), 1, "slideatatime moving 3 elements by 2 items");
 
 $it = slideatatime 1, 3, @x;
-@r = ();
+@r  = ();
 while (my @vals = $it->())
 {
     push @r, "@vals";
@@ -32,7 +32,7 @@ is(is_deeply(\@r, ['a b c', 'b c d', 'c d e', 'd e f', 'e f g', 'f g', 'g']), 1,
 
 my @a = (1 .. 1000);
 $it = slideatatime 1, 1, @a;
-@r = ();
+@r  = ();
 while (my @vals = &$it)
 {
     push @r, @vals;
@@ -41,7 +41,7 @@ is(is_deeply(\@r, \@a), 1, "slideatatime as natatime with 1 element");
 
 leak_free_ok(
     slideatatime => sub {
-        my @y = 1;
+        my @y  = 1;
         my $it = slideatatime 2, 2, @y;
         while (my @vals = $it->())
         {
